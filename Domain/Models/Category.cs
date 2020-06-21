a1742f94a40832e701b73de338a00183e24a448f
Aleksandr Klochkov <klochkov1w@gmail.com>
2020-06-21 22:37:18 +0300

    Basic models added

---
+++ Domain/Models/Category.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace yetAnoterSupermarketApi.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
